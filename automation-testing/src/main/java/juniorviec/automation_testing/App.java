bdd83b05cd70d3f4508782b27e97c0ecf8d205fd
Pham Hong Phuc <phuc.pham@student.passerellesnumeriques.org>
2018-10-18 20:22:27 +0700

    Create Maven project

---
+++ automation-testing/src/main/java/juniorviec/automation_testing/App.java
@@ -0,0 +1,13 @@
+package juniorviec.automation_testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
