824914ad5d7d2126c4062303edfcd012bbd940a4
Pham Hong Phuc <phuc.pham@student.passerellesnumeriques.org>
2018-11-07 01:16:27 +0700

    Read data file

--- automation-testing/src/test/java/runTests/Register.java
+++ automation-testing/src/test/java/runTests/Register.java
@@ -1,5 +1,12 @@
 package runTests;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,12 +16,41 @@ import org.testng.annotations.Test;
 import net.serenitybdd.core.annotations.findby.By;
 
 public class Register {
+	private static String dirData = "C:\\Users\\LAPTOP88\\Documents\\JuniorViec\\juniorviec-automation-web\\automation-testing\\data.xlsx";
 
 	private WebDriver driver;
 	private WebDriverWait wait;
 
+	// Get username
+	public static String getUserName() throws Exception {
+		File src = new File(dirData);
+		FileInputStream fis = new FileInputStream(src);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0); // Read data from Sheet1
+		String username = sheet.getRow(1).getCell(0).getStringCellValue();
+		System.out.println("Username: " + username);
+		return username;
+	}
+
+	// Get password
+	public static String getPassword() throws Exception {
+		File src = new File(dirData);
+		FileInputStream fis = new FileInputStream(src);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0); // Read data from Sheet1
+		DataFormatter dataFormartter = new DataFormatter();
+		String pass = dataFormartter.formatCellValue(sheet.getRow(1).getCell(1));
+		System.out.println("Password: " + pass);
+		return pass;
+	}
+
 	@Test
-	public void TestRegisterSuccessfully() throws InterruptedException {
+	public void TestRegisterSuccessfully() throws Exception {
+		
+		String username = getUserName();
+		String password = getPassword();
+		String passwordConfirm = getPassword();
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		wait = new WebDriverWait(driver, 100);
@@ -24,19 +60,20 @@ public class Register {
 		driver.get("https://juniorviec.com/register");
 
 		// Enter user's information
-		driver.findElement(By.xpath("//*[contains(@name, 'email')]")).sendKeys("phuc.pham9977+3@gmail.com");
-		driver.findElement(By.xpath("//*[@id='password']")).sendKeys("1234567890");
-		driver.findElement(By.xpath("//*[@id='cpassword']")).sendKeys("1234567890");
+		driver.findElement(By.xpath("//*[contains(@name,'email')]")).sendKeys(username);
+		driver.findElement(By.xpath("//*[@id='password']")).sendKeys(password);
+		driver.findElement(By.xpath("//*[@id='cpassword']")).sendKeys(passwordConfirm);
 		driver.findElement(By.xpath("//*[@id='firstname']")).sendKeys("Pham");
 		driver.findElement(By.xpath("//*[@id='lastname']")).sendKeys("Phuc");
-		
-//		Click [Sign up] button
+
+		// Click [Sign up] button
 		driver.findElement(By.xpath("//*[contains(@name, 'submit')]")).click();
-		
+
 		Thread.sleep(2000);
-		Assert.assertTrue(driver.findElement(By.xpath("//*[contains(@class, 'tab-content')]")).getText().contains("phuc.pham9977+3@gmail.com"), "You have just registered successfully!");
-		
-		//Close window
+		Assert.assertTrue(driver.findElement(By.xpath("//*[contains(@class,'tab-content')]")).getText()
+				.contains(username), "You have just registered successfully!");
+
+		// Close window
 		driver.quit();
 	}
 
